df8711399467b4b5519cc043d0816cf11b0b78c5
Amir <50082485+erxson@users.noreply.github.com>
2022-07-28 16:15:48 +0300

    Update JoinListener.java

--- src/main/java/com/james090500/VelocityWhitelist/listeners/JoinListener.java
+++ src/main/java/com/james090500/VelocityWhitelist/listeners/JoinListener.java
@@ -1,6 +1,7 @@
 package com.james090500.VelocityWhitelist.listeners;
 
 import com.james090500.VelocityWhitelist.VelocityWhitelist;
+import com.james090500.VelocityWhitelist.commands.CommandHandler;
 import com.james090500.VelocityWhitelist.config.Configs;
 import com.james090500.VelocityWhitelist.helpers.WhitelistHelper;
 import com.velocitypowered.api.event.PostOrder;
@@ -11,11 +12,15 @@ import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
+import java.util.concurrent.CompletableFuture;
+
 public class JoinListener {
+    private VelocityWhitelist velocityWhitelist;
 
     @Subscribe(order = PostOrder.EARLY)
     public void onPlayerJoin(LoginEvent event) {
         Player player = event.getPlayer();
+        Configs.loadConfigs(velocityWhitelist);
         if(Configs.getConfig().isEnabled()) {
             if(!WhitelistHelper.check(player)) {
                 TextComponent kickMessage = LegacyComponentSerializer.legacyAmpersand().deserialize(Configs.getConfig().getMessage());
@@ -23,5 +28,4 @@ public class JoinListener {
             }
         }
     }
-
 }
